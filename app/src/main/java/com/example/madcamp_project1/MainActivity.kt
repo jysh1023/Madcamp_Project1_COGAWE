ef74b3b115ec73dee3ea6b2a92fab1eadbc94daf
junseo <cloneot@gmail.com>
2023-06-30 16:21:27 +0900

    Add View Binding

--- app/src/main/java/com/example/madcamp_project1/MainActivity.kt
+++ app/src/main/java/com/example/madcamp_project1/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.madcamp_project1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.madcamp_project1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _bind: ActivityMainBinding? = null
+    private val bind get() = _bind!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        _bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _bind = null
     }
 }

b23dc09be0402b6177b77b159ebc789608131c5e
Seonghyun Yang <seonghyuny1023@naver.com>
2023-07-01 15:01:30 +0900

    Edit Contact Item & Add Action Bar

--- app/src/main/java/com/example/madcamp_project1/contact/ContactData.kt
+++ app/src/main/java/com/example/madcamp_project1/contact/ContactData.kt
@@ -1,9 +1,13 @@
 package com.example.madcamp_project1.contact
 
-import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class ContactData (
-    @SerializedName("name") val name: String,
-    @SerializedName("contact") val contact: String
+    @SerializedName("name") var name: String,
+    @SerializedName("contact") var contact: String
+
+
 )
+
+
+

c6fa8fb9b0d17d0d3d256b9e15af43d037028700
Seonghyun Yang <seonghyuny1023@naver.com>
2023-07-05 02:05:55 +0900

    Add call connection, edit item detail layout

--- app/src/main/java/com/example/madcamp_project1/contact/ContactFragment.kt
+++ app/src/main/java/com/example/madcamp_project1/contact/ContactFragment.kt
@@ -1,22 +1,14 @@
 package com.example.madcamp_project1.contact
 
-import android.content.Intent
-import android.content.Intent.getIntent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
-import android.widget.Toast
-import androidx.fragment.app.FragmentTransaction
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.madcamp_project1.R
 import com.example.madcamp_project1.databinding.FragmentContactBinding
 import com.google.gson.Gson
 import java.io.IOException
--- app/src/main/java/com/example/madcamp_project1/contact/NewContactDialog.kt
+++ app/src/main/java/com/example/madcamp_project1/contact/NewContactDialog.kt
@@ -1,20 +1,13 @@
 package com.example.madcamp_project1.contact
 
-import android.content.DialogInterface
-import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.setFragmentResult
-import com.example.madcamp_project1.MainActivity
-import com.example.madcamp_project1.R
 import com.example.madcamp_project1.databinding.NewContactDialogBinding
 
 

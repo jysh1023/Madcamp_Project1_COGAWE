48266ee63eed8ff3288bf46ff53cf30eb609ee10
junseo <cloneot@gmail.com>
2023-07-05 16:03:32 +0900

    fix(weather): fix api timeout error

--- app/src/main/java/com/example/madcamp_project1/util/Constants.kt
+++ app/src/main/java/com/example/madcamp_project1/util/Constants.kt
@@ -3,7 +3,7 @@ package com.example.madcamp_project1.util
 class Constants {
     companion object {
         const val API_KEY = "QK%2Bve1T4wuXNemP1vHO%2BWIhGvnuIq1vWC%2F4MYhjPOTpbafLNXsa1nipbwxAGqjMocQZfcxsMrTuxnlvbcCin6w%3D%3D"
-        const val BASE_URL = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
+        const val BASE_URL = "https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/"
         const val KAIST_NX = 66
         const val KAIST_NY = 101
     }
--- app/src/main/java/com/example/madcamp_project1/weather/RetrofitServiceImpl.kt
+++ app/src/main/java/com/example/madcamp_project1/weather/RetrofitServiceImpl.kt
@@ -1,12 +1,20 @@
 package com.example.madcamp_project1.weather
 
 import com.example.madcamp_project1.util.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object RetrofitServiceImpl {
+    val client: OkHttpClient = OkHttpClient.Builder()
+        .connectTimeout(0, TimeUnit.SECONDS)
+        .readTimeout(0, TimeUnit.SECONDS)
+        .build()
+
     private val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .client(client)
 //        .addCallAdapterFactory(RxJava3?)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
